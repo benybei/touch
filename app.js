415c2efd1600263537d6cf76ad4c2c47f82db068
mahamadou traore <benybei@hotmail.fr>
2012-12-09 16:20:00 -0500

    creation dune nouvel view pour le Help.js avec ajout de limage help

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ Ext.application({
         'Ext.MessageBox'
     ],
 
-    views: ['Main','Home','Contact','Blog'],
+    views: ['Main','Home','Contact','Blog','Help'],
 
     icon: {
         '57': 'resources/icons/Icon.png',
--- app/view/Main.js
+++ app/view/Main.js
@@ -1,24 +1,23 @@
 Ext.define('MyApp.view.Main', {
-    extend: 'Ext.tab.Panel',
-    
-    requires: [
-        'Ext.TitleBar',
-     
-    ],
-    config: {
-        tabBarPosition: 'bottom',
-/* pour laffichage du panneau welcom et get star*/
-        items: [
-                {
-                	xtype:'homepanel',
-                },
-                {
-                	xtype:'blog',
-                	style: 'border: none',
-                },
-                {
-                	xtype:'contactform',
-                }
-                ]
-    }
+	extend : 'Ext.tab.Panel',
+
+	requires : [ 'Ext.TitleBar',
+
+	],
+	config : {
+		tabBarPosition : 'bottom',
+		/* pour laffichage du panneau welcom et get star */
+		items : [ {
+			xtype : 'homepanel',
+		}, {
+			xtype : 'blog',
+			style : 'border: none',
+		}, {
+			xtype : 'contactform',
+		}, {
+			xtype : 'helppanel',
+		}
+
+		]
+	}
 });
